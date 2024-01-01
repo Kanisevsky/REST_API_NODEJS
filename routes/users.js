6b45b6854ec923b9f0ac6a4eb3427ea87bfbebce
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-01 20:39:02 +0000

    clearing the code by using controllers

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import {
+  getUsers,
+  getUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} from "./controllers/users.js";
+
 const router = express.Router();
 
 const users = [
@@ -8,27 +15,10 @@ const users = [
   { firstName: "David", lastName: "Murphy", age: 38 },
 ];
 
-router.get("/", (req, res) => {
-  res.send(users);
-});
-
-router.post("/", (req, res) => {
-  const user = req.body;
-
-  users.push({ ...user, id: uuidv4() });
-  res.send(`Users with the username ${user.firstName} added to the database!`);
-});
-
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const foundUser = users.find((user) => user.id === id);
-  res.send(foundUser);
-});
-
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  users = users.filter((user) => user.id !== id);
-  res.send(`User with id: ${id} deleted from database`);
-});
+router.get("/", getUsers);
+router.post("/", createUser);
+router.get("/:id", getUser);
+router.delete("/:id", deleteUser);
+router.patch("/:id", updateUser);
 
 export default router;
